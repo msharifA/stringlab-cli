46b01990cc4b7670425f0b74ba2adc4b298537fb
msharifA <2376659@brunel.ac.uk>
2025-08-05 16:50:02 +0100

    test: add JUnit tests and GitHub Actions CI

--- src/test/java/io/github/msharifa/AppTest.java
+++ src/test/java/io/github/msharifa/AppTest.java
@@ -1,20 +1,55 @@
-package io.github.msharifa;
-
-import static org.junit.Assert.assertTrue;
+package io.github.msharifa.stringlab;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.util.Map;
+
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAnagramTrue() {
+        assertTrue(App.isAnagram("listen", "silent"));
+    }
+
+    @Test
+    public void testAnagramFalse() {
+        assertFalse(App.isAnagram("hello", "world"));
+    }
+
+    @Test
+    public void testPalindromeTrue() {
+        assertTrue(App.isPalindrome("racecar"));
+    }
+
+    @Test
+    public void testPalindromeFalse() {
+        assertFalse(App.isPalindrome("apple"));
+    }
+
+    @Test
+    public void testCharFreq() {
+        Map<Character, Integer> freq = App.countLetters("aabbc");
+        assertEquals((Integer)2, freq.get('a'));
+        assertEquals((Integer)2, freq.get('b'));
+        assertEquals((Integer)1, freq.get('c'));
+    }
+
+    @Test
+    public void testDedup() {
+        String result = App.removeDuplicates("mississippi");
+        assertEquals("misp", result);
+    }
+
+    @Test
+    public void testCaesarEncrypt() {
+        String result = App.caesarCipher("abc", 3);
+        assertEquals("def", result);
+    }
+
+    @Test
+    public void testCaesarDecrypt() {
+        String result = App.caesarCipher("def", -3);
+        assertEquals("abc", result);
     }
 }
